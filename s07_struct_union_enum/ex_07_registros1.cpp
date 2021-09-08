268b564c7af88161c84a7790e5c8a97a051750e5
Bruno Monteiro <bruno84@gmail.com>
2021-09-08 14:55:21 -0300

    correcao na meet

--- s07_struct_union_enum/ex_07_registros1.cpp
+++ s07_struct_union_enum/ex_07_registros1.cpp
@@ -7,55 +7,52 @@ using namespace std;
 // DECLARAÇÃO DO REGISTRO E DO NOVO TIPO
 struct sFicha
 {
+	// Campos:
     string nome;
     float peso;
     float altura;
     float imc;
 };
 
+void exibirFicha(sFicha ficha)
+{
+	cout << endl << "------ EXIBINDO REGISTRO DE: " << ficha.nome << " ------ \n" ;
+    cout << "Peso: " << ficha.peso << endl;
+    cout << "Altura: " << ficha.altura << endl;
+    cout << "IMC: " << ficha.imc << endl<<endl;
+}
+
 int main()
 {
-    int i;
-    sFicha fichaAcademia;
-    sFicha fichaMedica = {"Bruno Monteiro", 78, 1.63, 0};
-    sFicha fichaMedicaCopia;
+	sFicha fichaAluno1 = {"Bruno Monteiro", 78, 1.63, 0};
+    sFicha fichaAluno2;
+    sFicha fichaAluno3;
 
 	// Pode ser feita atribuicao, como qq variavel
-	fichaMedicaCopia = fichaMedica;
+	fichaAluno2 = fichaAluno1;
 
     // ACESSANDO CONTEÚDOS DOS CAMPOS DO REGISTRO
-    cout << endl << "------ EXIBINDO REGISTRO DE: " << fichaMedicaCopia.nome << " ------ \n" ;
-    cout << "Peso: " << fichaMedicaCopia.peso << endl;
-    cout << "Altura: " << fichaMedicaCopia.altura << endl;
-    cout << "IMC: " << fichaMedicaCopia.imc << endl;
+	exibirFicha(fichaAluno1);
 
     // ACESSANDO CONTEÚDOS DOS CAMPOS DO REGISTRO
-    cout << endl << "------ EXIBINDO REGISTRO DE: " << fichaMedica.nome << " ------ \n" ;
-    cout << "Peso: " << fichaMedica.peso << endl;
-    cout << "Altura: " << fichaMedica.altura << endl;
-    cout << "IMC: " << fichaMedica.imc << endl<<endl;
-
+	exibirFicha(fichaAluno2);
+    
     // ATRIBUIÇÕES DOS CAMPOS DO REGISTRO
     cout << "Informe o nome da pessoa: ";
-    getline( cin, fichaAcademia.nome );
+    getline( cin, fichaAluno3.nome );
     
-    cout << "Informe o peso de: " << fichaAcademia.nome << ": ";
-    cin >> fichaAcademia.peso;
+    cout << "Informe o peso de: " << fichaAluno3.nome << ": ";
+    cin >> fichaAluno3.peso;
    
-    cout << "Informe a altura de: " << fichaAcademia.nome << ": ";
-    cin >> fichaAcademia.altura;
+    cout << "Informe a altura de: " << fichaAluno3.nome << ": ";
+    cin >> fichaAluno3.altura;
 
-	if(fichaAcademia.altura != 0) {
-    	fichaAcademia.imc = fichaAcademia.peso / (fichaAcademia.altura * fichaAcademia.altura);
+	if(fichaAluno3.altura != 0) {
+    	fichaAluno3.imc = fichaAluno3.peso / (fichaAluno3.altura * fichaAluno3.altura);
 	}
+
     // ACESSANDO CONTEÚDOS DOS CAMPOS DO REGISTRO
-    cout << endl << "------ EXIBINDO REGISTRO DE: " << fichaAcademia.nome << " ------ \n" ;
-    cout << "Peso: " << fichaAcademia.peso << endl;
-    cout << "Altura: " << fichaAcademia.altura << endl;
-    cout << "IMC: " << fichaAcademia.imc;
-
-
-	
+	exibirFicha(fichaAluno3);
 
     printf("\n\n");
     system("pause");
